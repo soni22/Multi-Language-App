c3b9c0d6a7341465f70bd4baddddbac3b9304e9c
Anshul <aakash.goel@gojavas.com>
2015-07-03 13:56:19 +0530

    no message

--- app/src/main/java/com/gojavas/tempola/activity/ProfileActivity.java
+++ app/src/main/java/com/gojavas/tempola/activity/ProfileActivity.java
@@ -1,21 +1,52 @@
 package com.gojavas.tempola.activity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import com.gojavas.tempola.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 /**
  * Created by GJS280 on 2/7/2015.
  */
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ImageView mProfileImageView;
+    private Spinner spinner_countryList;
+    public static final int REQUEST_CAMERA =1 ;
+    public static final int SELECT_FILE =2 ;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,9 +54,11 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         registerViews();
-
+        setUpClickListners();
         loadProfileImage();
+
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -37,6 +70,72 @@ public class ProfileActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
+    @Override
+    public void onClick(View v) {
+
+        switch(v.getId()){
+
+            case R.id.profile_imageview:
+                selectImage();
+                break;
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_CAMERA) {
+                Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                thumbnail.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+
+                File destination = new File(Environment.getExternalStorageDirectory(),
+                        System.currentTimeMillis() + ".jpg");
+
+                FileOutputStream fo;
+                try {
+                    destination.createNewFile();
+                    fo = new FileOutputStream(destination);
+                    fo.write(bytes.toByteArray());
+                    fo.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                mProfileImageView.setImageBitmap(thumbnail);
+
+            } else if (requestCode == SELECT_FILE) {
+                Uri selectedImageUri = data.getData();
+                String[] projection = {MediaStore.MediaColumns.DATA};
+                Cursor cursor = managedQuery(selectedImageUri, projection, null, null,
+                        null);
+                int column_index = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+                cursor.moveToFirst();
+
+                String selectedImagePath = cursor.getString(column_index);
+
+                Bitmap bm;
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inJustDecodeBounds = true;
+                BitmapFactory.decodeFile(selectedImagePath, options);
+                final int REQUIRED_SIZE = 200;
+                int scale = 1;
+                while (options.outWidth / scale / 2 >= REQUIRED_SIZE
+                        && options.outHeight / scale / 2 >= REQUIRED_SIZE)
+                    scale *= 2;
+                options.inSampleSize = scale;
+                options.inJustDecodeBounds = false;
+                bm = BitmapFactory.decodeFile(selectedImagePath, options);
+
+                mProfileImageView.setImageBitmap(bm);
+            }
+        }
+    }
     private void registerViews() {
         final Toolbar toolbar = (Toolbar) findViewById(R.id.profile_toolbar);
         setSupportActionBar(toolbar);
@@ -45,9 +144,125 @@ public class ProfileActivity extends AppCompatActivity {
         CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.profile_collapsing_toolbar);
 
         mProfileImageView = (ImageView) findViewById(R.id.profile_imageview);
+        spinner_countryList=(Spinner)findViewById(R.id.profile_countrycode);
+
+   }
+
+    private void setUpClickListners() {
+
+        mProfileImageView.setOnClickListener(this);
+
+        ArrayList<String> countryList=parseCountryCodes();
+
+        ArrayAdapter<String> adapter_state = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,countryList);
+        adapter_state
+                .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_countryList.setAdapter(adapter_state);
+        spinner_countryList.setSelection(79);
+
+        spinner_countryList
+                .setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+                                               @Override
+                                               public void onItemSelected(AdapterView<?> parent,
+                                                                          View view, int position, long id) {
+                                                   // TODO Auto-generated method stub
+
+//                                                   Toast.makeText(ProfileActivity.this, position + "", Toast.LENGTH_LONG).show();
+                                               }
+
+                                               @Override
+                                               public void onNothingSelected(AdapterView<?> parent) {
+                                                   // TODO Auto-generated method stub
+
+                                               }
+                                           }
+
+
+                );
+
     }
+
 
     private void loadProfileImage() {
 
+    }
+
+    /**
+     * Get Country codes from countrycodes.txt in assests
+     * @return
+     */
+    public ArrayList<String> parseCountryCodes() {
+        String response = "";
+        ArrayList<String> list = new ArrayList<String>();
+        try {
+            StringBuffer sb = new StringBuffer();
+            BufferedReader br = null;
+            try {
+                br = new BufferedReader(new InputStreamReader(getAssets().open(
+                        "countrycodes.txt")));
+                String temp;
+                while ((temp = br.readLine()) != null) {
+                    sb.append(temp);
+                    //System.out.println("br::" + br);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    br.close(); // stop reading
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            response = sb.toString();
+
+
+            JSONArray array = new JSONArray(response);
+            for (int i = 0; i < array.length(); i++) {
+                JSONObject object = array.getJSONObject(i);
+                list.add(object.getString("phone-code") + " "
+                        + object.getString("name"));
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    private void selectImage() {
+        final CharSequence[] items = { "Take Photo", "Choose from Library",
+                "Cancel" };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(ProfileActivity.this);
+        builder.setTitle("Add Photo!");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+
+
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (items[item].equals("Take Photo")) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, REQUEST_CAMERA);
+                } else if (items[item].equals("Choose from Library")) {
+                    Intent intent = new Intent(
+                            Intent.ACTION_PICK,
+                            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    intent.setType("image/*");
+                    startActivityForResult(
+                            Intent.createChooser(intent, "Select File"),
+                            SELECT_FILE);
+                } else if (items[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
     }
 }
