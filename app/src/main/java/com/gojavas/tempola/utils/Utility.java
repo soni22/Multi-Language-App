8520dc50aff6d78c40e808015b255bd1d773b995
niteshkhatri <khatrinitesh1991@gmail.com>
2015-07-09 15:09:29 +0530

    registration

--- app/src/main/java/com/gojavas/tempola/utils/Utility.java
+++ app/src/main/java/com/gojavas/tempola/utils/Utility.java
@@ -1,13 +1,26 @@
 package com.gojavas.tempola.utils;
 
 import android.content.Context;
+import android.telephony.TelephonyManager;
 import android.view.Gravity;
 import android.widget.Toast;
+
+import com.gojavas.tempola.application.TempolaApplication;
 
 /**
  * Created by gjs331 on 7/3/2015.
  */
 public class Utility {
+
+    /**
+     * Get device id
+     * @return
+     */
+    public static String getDeviceId() {
+        Context context = TempolaApplication.getInstance();
+        TelephonyManager telephonyManager = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
+        return telephonyManager.getDeviceId();
+    }
 
 
     /**
